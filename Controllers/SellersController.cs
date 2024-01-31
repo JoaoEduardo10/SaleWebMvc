7e4d69c0af62519b7a1a76f6b8da726c3f725e0e
João Eduardo <joaoeduardoj920@gmail.com>
2024-01-31 13:48:58 -0300

    feat: Adding movie-view-controller for Sellers

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly ILogger<SellersController> _logger;
+
+        public SellersController(ILogger<SellersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
